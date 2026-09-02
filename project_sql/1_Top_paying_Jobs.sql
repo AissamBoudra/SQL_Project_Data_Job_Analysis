466b4a2c915862a04a24010f0ff4e31a13d860d9
AissamBoudra <bmaissam@yahoo.fr>
2026-09-02 14:00:22 +0100

    commit_2_26_09_02

--- project_sql/1_Top_paying_Jobs.sql
+++ project_sql/1_Top_paying_Jobs.sql
@@ -1,14 +1,17 @@
 SELECT 
-      job_id,
-       company_id,
-       job_title,
-       job_location,
-       job_country,
-       job_schedule_type,
-       salary_year_avg
-       job_posted_date,
+      j.job_id,
+       j.company_id,
+       j.job_title,
+       c.name as compagny_name,
+       j.job_location,
+       j.job_country,
+       j.job_schedule_type,
+       j.salary_year_avg,
+       j.job_posted_date
 
-FROM public.job_postings_fact
-where job_title_short = 'Data Analyst' and salary_year_avg is not null and job_work_from_home = 'True'
-order by salary_year_avg DESC   
+FROM public.job_postings_fact as j
+inner join company_dim as c on j.company_id = c.company_id
+where j.job_title_short = 'Data Analyst' and j.salary_year_avg is not null and j.job_work_from_home = 'True'
+
+order by j.salary_year_avg DESC   
 LIMIT 10;
